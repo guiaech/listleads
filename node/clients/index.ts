ac027b4fceafd0ffc53f6898964cbf0be810ec2a
Guilherme Andrade e Chaves <guilherme@live.it>
2021-08-29 13:36:17 -0300

    melhorias

--- node/clients/index.ts
+++ node/clients/index.ts
@@ -1,3 +1,8 @@
 import { IOClients } from '@vtex/api'
+import {Catalog} from '@vtex/clients'
 
-export class Clients extends IOClients {}
+export class Clients extends IOClients {
+    public get status() {
+      return this.getOrSet('catalog', Catalog)
+    }
+  }
--- react/LeadsList.tsx
+++ react/LeadsList.tsx
@@ -4,18 +4,18 @@ import { useState, useEffect } from 'react';
 
 import api from './api';
 
-import 'vtex-tachyons'
+//import 'vtex-tachyons'
 
 
 interface LeadsList {}
 
 interface Leads{
-  leds?: string
+  leads: string
   empresa: string
   id: string
   name: string
   number: string
-  recado?: string
+  recado: string
 }
 
 
@@ -27,9 +27,9 @@ const LeadsList: StorefrontFunctionComponent<LeadsList> = ({}) => {
   async function getLeads(){
     const response = await api.get('/items')
 
-    setLeads(response.data.prospectos)
+    setLeads(response.data.Items)
     
-    console.log(response.data.prospectos)
+    console.log(response.data.Items)
   }
 
   useEffect( () => {
@@ -38,36 +38,36 @@ const LeadsList: StorefrontFunctionComponent<LeadsList> = ({}) => {
 
 
   return (
-      <div className="w-80 center pt4">
-        <div className="">
-          <div className="">
+      <div>
+        <div >
           <div>
-            <h1 className="f1">Lista de Leads</h1>
+          <div>
+            <h1 >Lista de Leads</h1>
           </div>
 
-          <div className="">
-            <table className="f6 mb5 w-100 center">
-              <thead className="">
-                <tr className="stripe-dark">
-                <th className="fw6 tl bb b--black-20 pt2 bg-white">empresa</th>
-                <th className="fw6 tl bb b--black-20 pt2 bg-white">Nome</th>
-                <th className="fw6 tl bb b--black-20 pt2 bg-white">E-mail</th>
-                <th className="fw6 tl bb b--black-20 pt2 bg-white">Telefone</th>
-                <th className="fw6 tl bb b--black-20 pt2 bg-white">Data de Cadastro</th>
-                <th className="fw6 tl bb b--black-20 pt2 bg-white">recado</th>
+          <div>
+            <table >
+              <thead>
+                <tr >
+                <th>empresa</th>
+                <th>Nome</th>
+                <th>E-mail</th>
+                <th>Telefone</th>
+                <th>Data de Cadastro</th>
+                <th>recado</th>
                 </tr>
               </thead>
 
-              <tbody className="lh-copy">
-              {leads.length > 0 ?
+              <tbody >
+              {leads.length> 0 ?
                 leads.map(lead => (
-                  <tr className="stripe-dark bb b--black-20"  key={lead.id}>
-                    <td className="tl bb b--black-20 pt2">{lead.id}</td>
-                    <td className="tl bb b--black-20 pt2">{lead.name}</td>
-                    <td className="tl bb b--black-20 pt2">{lead.number}</td>
-                    <td className="tl bb b--black-20 pt2">{lead.empresa}</td>
-                    <td className="tl bb b--black-20 pt2">{lead.leds === '' ? '-': lead.leds}</td>
-                    <td className="tl bb b--black-20 pt2">{lead.recado}</td>
+                  <tr key={lead.id}>
+                    <td>{lead.id}</td>
+                    <td>{lead.name}</td>
+                    <td>{lead.number}</td>
+                    <td>{lead.empresa}</td>
+                    <td>{lead.leads}</td>
+                    <td>{lead.recado}</td>
                   </tr>
                 )) : ''}
               </tbody>
