54c2cca7f1e41d7b8b4d9d567fe6b7b249f8d2af
Guilherme Andrade e Chaves <guilherme@live.it>
2021-08-29 21:53:20 -0300

    upload

--- README.md
+++ README.md
@@ -1,3 +1,24 @@
-# Store Block course template
 
-Here you'll learn how to create awesome Store Framework blocks!
+
+# COMPONENTE REACT (LISTA DE CLIENTES)
+<!-- ALL-CONTRIBUTORS-BADGE:START - Do not remove or modify this section -->
+[![All Contributors](https://img.shields.io/badge/all_contributors-10-orange.svg?style=flat-square)](#contributors)
+<!-- ALL-CONTRIBUTORS-BADGE:END -->
+
+## O que isto faz?
+
+Este app fornece uma lista para ser integrada em um site, os dados que são exebidos nessa lista são da API GATEWAY da AWS busca os dados no banco de dados DynamoDB.
+
+## Dependências
+- AWS API Externa
+  
+
+
+# Colaboradores ✨
+<table>
+  <tr>
+    <td align="center"><a href="https://github.com/guiaech"><img src="https://avatars.githubusercontent.com/u/83043492?v=4" width="100px;" alt=""/><br /><sub><b>Guilherme Andrade</b></sub></a></td>
+    <td align="center"><a href="https://github.com/felipesiqg"><img src="https://avatars.githubusercontent.com/u/68981603?v=4" width="100px;" alt=""/><br /><sub><b>Felipe Siqueira</b></sub></a></td>
+    <td align="center"><a href="https://github.com/Steffany-Martins"><img src="https://avatars.githubusercontent.com/u/59336147?v=4" width="100px;" alt=""/><br /><sub><b>Steffany Martins Soares</b></sub></a></td>
+  </tr>
+</table>
--- react/LeadsList.tsx
+++ react/LeadsList.tsx
@@ -38,46 +38,46 @@ const LeadsList: StorefrontFunctionComponent<LeadsList> = ({}) => {
 
 
   return (
-      <div>
-        <div >
-          <div>
-          <div>
-            <h1 >Lista de Leads</h1>
-          </div>
-
-          <div>
-            <table >
-              <thead>
-                <tr >
-                <th>empresa</th>
-                <th>Nome</th>
-                <th>E-mail</th>
-                <th>Telefone</th>
-                <th>Data de Cadastro</th>
-                <th>recado</th>
-                </tr>
-              </thead>
-
-              <tbody >
-              {leads.length> 0 ?
-                leads.map(lead => (
-                  <tr key={lead.id}>
-                    <td>{lead.id}</td>
-                    <td>{lead.name}</td>
-                    <td>{lead.number}</td>
-                    <td>{lead.empresa}</td>
-                    <td>{lead.leads}</td>
-                    <td>{lead.recado}</td>
-                  </tr>
-                )) : ''}
-              </tbody>
-            </table>
-          </div>
-          </div>
+    <div className="w-80 center pt4">
+      <div className="">
+        <div className="">
+        <div>
+          <h1 className="f1">Lista de Leads</h1>
         </div>
 
-    </div>
-  )}
+        <div className="">
+          <table className="f6 mb5 w-100 center">
+            <thead className="">
+              <tr className="stripe-dark">
+              <th className="bg-white fw6 tl bb b--black-20 pt2">E-mail</th>
+              <th className="bg-white fw6 tl bb b--black-20 pt2">Nome</th>
+              <th className="bg-white fw6 tl bb b--black-20 pt2">Telefone</th>
+              <th className="bg-white fw6 tl bb b--black-20 pt2">Empresa</th>
+              <th className="bg-white fw6 tl bb b--black-20 pt2">Data de Cadastro</th>
+              <th className="bg-white fw6 tl bb b--black-20 pt2">recado</th>
+              </tr>
+            </thead>
+
+            <tbody className="lh-copy">
+            {leads.length > 0 ?
+              leads.map(lead => (
+                <tr className="stripe-dark bb b--black-20"  key={lead.id}>
+                  <td className="tl bb b--black-20 pt2">{lead.id}</td>
+                  <td className="tl bb b--black-20 pt2">{lead.name}</td>
+                  <td className="tl bb b--black-20 pt2">{lead.number}</td>
+                  <td className="tl bb b--black-20 pt2">{lead.empresa}</td>
+                  <td className="tl bb b--black-20 pt2">{lead.leads}</td>
+                  <td className="tl bb b--black-20 pt2">{lead.recado}</td>
+                </tr>
+              )) : ''}
+            </tbody>
+          </table>
+        </div>
+        </div>
+      </div>
+
+  </div>
+)}
 
 
 LeadsList.schema = {
